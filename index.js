c8f9c8fc683999132d3cbb7efb7a9250314b4a1b
fuad <ahmadfuadoorakam@gmail.com>
2024-07-17 06:15:27 +0530

    callback

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Running..1");
+console.log("Start");
+
+const dataApi = (callback) => {
+  setTimeout(() => {
+    callback("fuad");
+  }, 3000);
+};
+
+dataApi((value) => {
+  console.log(value);
+});
+
+console.log("Finish");
