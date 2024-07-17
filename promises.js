526069ea95d35768efabfac341b3bbcf68045395
fuad <ahmadfuadoorakam@gmail.com>
2024-07-17 12:09:13 +0530

    async and await

--- promises.js
+++ promises.js
@@ -1,22 +1,27 @@
-const dataApi = (num) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(num);
-    }, 1000);
-  });
-};
-const dataApi1 = (num) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(num);
-    }, 1000);
-  });
+// const promise = new Promise((resolve, reject) => {
+//   resolve(["fuad", "aju", "basi"]);
+//   reject("Api Failed");
+// });
+
+const genericFetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
-dataApi(5)
-  .then((result) => {
-    return dataApi1(result + 4);
-  })
-  .then((response) => {
-    console.log(response);
-  });
+const fetchData = async () => {
+  try {
+    const value = await genericFetchData(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    console.log(value);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+fetchData();
